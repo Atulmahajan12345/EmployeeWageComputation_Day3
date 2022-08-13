1b6a791bcc88a5916f13813a9a3d9ba5edc27eed
Atulmahajan12345 <vishal.business0504@gmail.com>
2022-08-13 14:03:30 +0530

    Updated:employee present or absent

---
+++ src/com/bridgelabz/empwage/checkAttendence.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.empwage;
+
+public class checkAttendence {
+public static final int IS_PRESENT = 1;
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Employee Wage Computation" );
+       
+        double empCheck = Math.floor(Math.random() * 10 ) % 2;
+        
+        if (empCheck == IS_PRESENT)
+        {
+        	System.out.println("Employee is Present");
+        }
+        else
+        {
+        	System.out.println("Employee is Absent");
+        }
+    }
+}
+
+
